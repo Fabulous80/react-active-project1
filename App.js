5eccae56ba9c259499c4b7e895c78ebd0a970017
Marcus Lim <marcz68@gmail.com>
2020-12-01 10:04:46 +0800

    change to text

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello World !</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFC300",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    color: "#C52C0B",
+    fontWeight: "bold",
+    fontSize: 60,
   },
 });
